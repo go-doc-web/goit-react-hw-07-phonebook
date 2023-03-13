1740cbda1d20197c3638edf73fa529b5d4d3a649
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-03-13 22:44:43 +0200

    end2

--- src/Redux/selectors.js
+++ src/Redux/selectors.js
@@ -6,17 +6,6 @@ export const getFilter = state => state.filter;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
-// export const getFilteredContacts = ({ contacts, filter }) => {
-//   if (!filter) {
-//     return contacts;
-//   }
-//   const normalizedFilter = filter.toLowerCase();
-//   const result = contacts.filter(({ name }) => {
-//     return name.toLowerCase().includes(normalizedFilter);
-//   });
-//   return result;
-// };
-
 export const getFilteredContacts = createSelector(
   [getContacts, getFilter],
   (contacts, query) => {
